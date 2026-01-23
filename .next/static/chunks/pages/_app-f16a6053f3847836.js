9b9acb88459f2bc786ad4dd2347c674b3aa73132
Mrwowow <morganvic01@gmail.com>
2026-01-23 11:20:57 +0100

    feat: Add StoreFooter component and layout type definitions
    - Implemented StoreFooter component for displaying store information, quick links, categories, and contact details.
    - Added type definitions for layout in app/layout.ts to ensure type safety for layout properties and functions.
    - Updated webpack hot-update files for new changes.

--- .next/static/chunks/pages/_app-f16a6053f3847836.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{7289:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(9988)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(7289),_(9201)}),_N_E=n.O()}]);
